7016262c87dcfb381fb6feead352b10506aa9c4b
collins njau <collinsnjau39@gmail.com>
2020-04-11 17:54:19 +0300

    Debug tests

--- src/estimator.js
+++ src/estimator.js
@@ -32,6 +32,7 @@ const severeCasesByTime = (infectionsByTime) => (15 / 100) * infectionsByTime;
 const hospitalBedsByTime = (severeCasesByRequestedTime, totalHospitalBeds) => {
   const availableBeds = (35 / 100) * totalHospitalBeds;
   const answer = Math.floor(availableBeds) - severeCasesByRequestedTime + 1;
+  console.log(Math.sign(answer));
   return Math.sign(answer) === -1 ? answer + 1 : answer;
 };
 
