a6c4b1229b277723fa6b08bf1a5689c8b4e7862b
collins njau <collinsnjau39@gmail.com>
2020-04-11 14:11:21 +0300

    Fix lint issues

--- src/estimator.js
+++ src/estimator.js
@@ -15,17 +15,30 @@
 
 const covid19ImpactEstimator = (data) => {
   // get the days
-  const {reportedCases} = data;
+  const { reportedCases } = data;
   // calculate the currently infected for impact and severe
-  const impactCurrentlyInfected = reportedCases*10
-  const severeCurrentlyInfected = reported*10
+  const impactCurrentlyInfected = reportedCases * 10;
+  const severeCurrentlyInfected = reportedCases * 10;
 
   // calculate the infectionsByRequestedTime
   let days;
-  const factor = Math.round(days/3)
-  const multiplier = Math.pow(2, factor)
-  const impactInfectionsByRequestedTime =impactCurrentlyInfected* multiplier
-  const severeInfectionsByRequestedTime = severeCurrentlyInfected*multiplier
+  const factor = Math.round(days / 3);
+  const multiplier = 2 ** factor;
+  const impactInfectionsByRequestedTime = impactCurrentlyInfected * multiplier;
+  const severeInfectionsByRequestedTime = severeCurrentlyInfected * multiplier;
+
+  const output = {
+    data: {}, // the input data you got
+    impact: {
+      impactCurrentlyInfected,
+      impactInfectionsByRequestedTime
+    }, // your best case estimation
+    severeImpact: {
+      severeCurrentlyInfected,
+      severeInfectionsByRequestedTime
+    } // your severe case estimation
+  };
+  return output;
 };
 
 export default covid19ImpactEstimator;
