dbae086270795cb8530f08d64b5fa19ee2f6c824
collins njau <collinsnjau39@gmail.com>
2020-04-13 23:26:32 +0300

    Add logs controller

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-
+const fs = require('fs');
+const path =  require('path');
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const covidRoutes = require("./src/routes/covid");
+
+/* eslint-disable no-underscore-dangle, no-console */
+const directoryName = path.resolve();
+global.basedir = directoryName;
+/* eslint-disable no-underscore-dangle, no-console */
+
+if (!fs.existsSync(path.join(directoryName, 'logs/requests.log'))) {
+  fs.mkdirSync('logs');
+}
+
+// create a write stream (in append mode)
+const requestsLogStream = fs.createWriteStream(path.join(directoryName, 'logs/requests.log'), { flags: 'a+' });
+morgan.token('response-time-ms', function getResponse(req, res) {
+  const time = this['response-time'](req, res, 0) < 10 ? `0${this['response-time'](req, res, 0)}ms` : `${this['response-time'](req, res, 0)}ms`;
+  return time;
+});
+app.use(morgan(':method\t:url\t:status\t:response-time-ms', { stream: requestsLogStream }));
 
 /**
  * Give access control
--- src/routes/covid.js
+++ src/routes/covid.js
@@ -7,6 +7,7 @@ const covidController = require('../controllers/covid');
 router.post('/', covidController.disease_estimator);
 router.post('/json', covidController.disease_estimator);
 router.post('/xml', covidController.disease_estimator_xml);
+router.get('/logs', covidController.get_logs);
 
 
 module.exports = router;
